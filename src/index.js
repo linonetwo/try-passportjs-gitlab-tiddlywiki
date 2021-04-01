c351f91dad2efa0bc3f88e5b8d6a51b62d8bcf2a
Lin Onetwo <linonetwo012@gmail.com>
2021-04-02 00:51:06 +0800

    feat: use cookies to verify user is trusted user

--- src/index.js
+++ src/index.js
@@ -5,20 +5,35 @@ import bodyParser from 'body-parser';
 import passport from 'passport';
 import GitLabStrategy from 'passport-gitlab2';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import cookieSession from 'cookie-session';
 import { startNodeJSWiki } from './wiki';
 
 require('dotenv').config();
 
 const port = process.env.PORT || 8000;
-const validUserName = 'linonetwo';
-let isValidUser = false;
 
 const app = express();
+app.set('trust proxy', 1); // trust first proxy
 app.use(cors());
 app.use('/static', express.static(path.join(__dirname, '..', 'static')));
 app.use(bodyParser.json());
 
+// tiddlywiki related auth info, only allow valid user to edit wiki
+const validUserName = process.env.VALID_USER; // for example 'linonetwo';
+const validUserAccessTokens = {}; // store accessToken from gitlab login, used to check user identity
+const cookieName = 'tiddlywiki-auth';
+// this will add a cookie to req.session
+app.use(
+  cookieSession({
+    name: cookieName,
+    keys: ['key1', 'key11'],
+  })
+);
+
+// set up passportjs, we get user's name from trusted identity source, in this case, gitlab
 app.use(passport.initialize());
+app.use(passport.session());
+// add `serializeUser` and `deserializeUser` otherwise there will be `Failed to serialize user into session`
 passport.serializeUser(function (user, done) {
   done(null, user);
 });
@@ -36,19 +51,20 @@ passport.use(
       baseURL: 'https://gitlab.com/',
     },
     function (accessToken, refreshToken, profile, cb) {
-      // TODO: use session manager to prevent multiple user interference with each other
-      isValidUser = accessToken && profile?.username === validUserName;
-      // DEBUG: console
-      console.log(`profile`, JSON.stringify(profile, undefined, '  '));
-      // DEBUG: console
-      console.log(`accessToken, refreshToken,`, accessToken, refreshToken);
-      cb(undefined, accessToken);
+      // if user is valid, we store the accessToken to local cache, and use it to check user later
+      if (accessToken && profile?.username === validUserName) {
+        validUserAccessTokens[accessToken] = true;
+
+        // second argument will be added to req.user, so we can get information in middlewares later
+        cb(undefined, { accessToken, userName: profile.username });
+      } else {
+        cb(new Error(`No a valid user ${profile?.username ?? '-'}`));
+      }
     }
   )
 );
 
 app.get('/auth/gitlab', passport.authenticate('gitlab'));
-
 app.get(
   `/${gitlabCallbackPath}`,
   passport.authenticate('gitlab', {
@@ -62,7 +78,13 @@ app.get(
 
 // skip tiddlywiki if is not login
 app.use(function (req, res, next) {
-  if (isValidUser) {
+  // if it is first time auth, we will get req.user, and no req.cookies
+  if (req?.user?.accessToken in validUserAccessTokens && req?.user?.userName === validUserName) {
+    // we add info to the cookie
+    req.session.accessToken = req.user.accessToken;
+    req.session.userName = req.user.userName;
+    next();
+  } else if (req.session.userName === validUserName && req.session.accessToken in validUserAccessTokens) {
     next();
   } else {
     res.redirect('/static/empty-wiki.html');
@@ -81,5 +103,6 @@ app.use(
 );
 startNodeJSWiki(tiddlyWikiConfig);
 
+app.set('host', '0.0.0.0');
 app.set('port', port);
 app.listen(app.get('port'), () => console.log(`Server is running on port ${app.get('port')}`));
